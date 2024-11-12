5ad628bb82e6f4e82bd7492d3ee7d5094a7c39f9
Andres Valverde <valan465@gmail.com>
2024-11-12 16:17:10 -0400

    segundo commit

--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,11 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    #...
+    x = 5
+    while x >= 0:
+        result.append(x)
+        x -= 1
+    print (result)
     return result  
+
+fn_hack_7()
--- hack_9.py
+++ hack_9.py
@@ -4,5 +4,10 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 
 def fn_hack_9():
     result = [1,2,3]
-    #...
-    return result  
+    x = 1
+    while x <= 5:
+        result.insert(x,'@')
+        x += 2
+    print(result)
+    return result   
+fn_hack_9()
