13d4be8f28e209bc80c14c0e7c0db8481a10101e
Andres Valverde <valan465@gmail.com>
2024-11-13 15:34:10 -0400

    done

--- hack_10.py
+++ hack_10.py
@@ -4,5 +4,18 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 
 def fn_hack_10():
     result = "fooziman"
-    #...
+    result = result.upper()
+    output = []
+
+    for i in result[0:]:
+        if i == 'O':
+            output.append('0')
+        elif i == 'I':
+            output.append('1')
+        elif i == 'A':
+            output.append('@')
+        else:
+            output.append(i)
+    print(output)
     return result  
+fn_hack_10()
